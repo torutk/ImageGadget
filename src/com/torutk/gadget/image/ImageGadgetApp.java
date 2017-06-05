135d75590cef8da942379c19bcd4dbd60f54194a
TAKAHASHI,Toru <torutk@gmail.com>
2017-06-05 20:54:26 +0900

    GadgetSupportライブラリをサブモジュールとして追加し、利用

--- src/com/torutk/gadget/image/ImageGadgetApp.java
+++ src/com/torutk/gadget/image/ImageGadgetApp.java
@@ -3,6 +3,8 @@
  */
 package com.torutk.gadget.image;
 
+import com.torutk.gadget.support.TinyGadgetSupport;
+import java.util.prefs.Preferences;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,6 +20,8 @@ public class ImageGadgetApp extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("ImageGadgetView.fxml"));
         Scene scene = new Scene(root);
+        
+        new TinyGadgetSupport(stage, Preferences.userNodeForPackage(this.getClass()));
         
         stage.setScene(scene);
         stage.show();
