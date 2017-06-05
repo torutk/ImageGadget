45feb7622cb29cde9183b1d364cc1e3e977b08c5
TAKAHASHI,Toru <torutk@gmail.com>
2017-06-05 20:39:25 +0900

    ローカルファイルシステムの画像を表示するガジェットの初期実装

---
+++ src/com/torutk/gadget/image/ImageGadgetApp.java
@@ -0,0 +1,33 @@
+/*
+ * © 2017 TAKAHASHI,Toru
+ */
+package com.torutk.gadget.image;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * ImageGadgetアプリケーションのエントリポイントを提供するクラス。
+ */
+public class ImageGadgetApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ImageGadgetView.fxml"));
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/com/torutk/gadget/image/ImageGadgetViewController.java
@@ -0,0 +1,76 @@
+/*
+ * © 2017 TAKAHASHI,Toru
+ */
+package com.torutk.gadget.image;
+
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Region;
+import javafx.util.Duration;
+
+/**
+ * ImageGadgeViewのコントローラクラス。
+ */
+public class ImageGadgetViewController implements Initializable {
+    
+    @FXML
+    private Region root;
+    @FXML
+    private ImageView imageView;
+    
+    private ImageGadgetViewModel model = new ImageGadgetViewModel(Paths.get("."));
+    
+    @FXML
+    private void handleDragOver(DragEvent event) {
+        Dragboard dragboard = event.getDragboard();
+        if (dragboard.hasFiles() && dragboard.getFiles().get(0).isDirectory()) {
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+        event.consume();
+    }
+    @FXML
+    private void handleDragDropped(DragEvent event) {
+        Dragboard dragboard = event.getDragboard();
+        if (dragboard.hasFiles() && dragboard.getFiles().get(0).isDirectory()) {
+            model = new ImageGadgetViewModel(Paths.get(dragboard.getFiles().get(0).toURI()));
+            nextImage(event);
+            event.setDropCompleted(true);
+        } else {
+            event.setDropCompleted(false);
+        }
+        event.consume();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        imageView.fitWidthProperty().bind(root.widthProperty());
+        imageView.fitHeightProperty().bind(root.heightProperty());
+        imageView.setPreserveRatio(true);
+        nextImage(new ActionEvent());
+        
+        Timeline timer = new Timeline();
+        timer.getKeyFrames().add(new KeyFrame(Duration.seconds(10), this::nextImage));
+        timer.setCycleCount(Timeline.INDEFINITE);
+        timer.play();
+    }
+    
+    private ImageGadgetViewModel getModel() {
+        return model;
+    }
+
+    private void nextImage(Event ev) {
+        getModel().next().ifPresent(imageView::setImage);
+    }
+       
+}
