039037221dcc56aae130a46c44367b3fcebcfed5
TAKAHASHI,Toru <torutk@gmail.com>
2018-12-01 22:35:02 +0900

    regenerate netbeans project with java application type for jdk11.

--- src/com/torutk/gadget/image/ImageGadgetApp.java
+++ src/com/torutk/gadget/image/ImageGadgetApp.java
@@ -27,11 +27,4 @@ public class ImageGadgetApp extends Application {
         stage.show();
     }
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
 }
---
+++ src/com/torutk/gadget/image/Main.java
@@ -0,0 +1,16 @@
+/*
+ *  Copyright © 2018 toru
+ */
+package com.torutk.gadget.image;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author toru
+ */
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(ImageGadgetApp.class, args);
+    }
+}
